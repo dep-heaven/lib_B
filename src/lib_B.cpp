c7b119372bfe85b8bff8a6dc42c5955a5e2b03fd
Markus Werle <daixtrose@users.noreply.github.com>
2021-03-25 03:33:45 +0100

    add libFreeAssange

--- src/lib_B.cpp
+++ src/lib_B.cpp
@@ -1,9 +1,12 @@
+#include <libFreeAssange/free_assange.hpp>
+
 #include <fmt/core.h>
 
 namespace lib_B {
 
 int fn_b()
 {
+    free_assange::doIt();
     fmt::print("The answer is {}.", 42);
     return 42;
 }
